c508f0888c86748880bdf7116e0656c4fe786464
pengshengsong <shengsongpeng@huangbaoche.com>
2016-10-19 11:25:50 +0800

    初始化代码

---
+++ 自定义基础框架（nav + tab）/自定义基础框架（nav + tab）/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  自定义基础框架（nav + tab）
+//
+//  Created by 彭盛凇 on 16/10/19.
+//  Copyright © 2016年 huangbaoche. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
